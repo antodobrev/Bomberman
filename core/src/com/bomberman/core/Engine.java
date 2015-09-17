0f9e1d2b2f524b5df6d8f8af0d28c3ccf2ebd34b
Ivaylo <atriumandvintrigol@gmail.com>
2015-09-17 12:24:25 +0300

    Player Box Free Space Added

--- core/src/com/bomberman/core/Engine.java
+++ core/src/com/bomberman/core/Engine.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.joints.FrictionJoint;
 
 public class Engine extends ApplicationAdapter {
 	private SpriteBatch batch;
@@ -20,40 +21,59 @@ public class Engine extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		// wall = new Texture("images/wall.png");
-		wall = new Texture("desktop/images/wall.png");
-		// background = new Texture("images/background.jpg");
-		background = new Texture("desktop/images/background.jpg");
-		// brickwall = new Texture("images/brickwall.jpg");
-		brickwall = new Texture("desktop/images/brickwall.jpg");
-
+		wall = new Texture("images/wall.png");
+		background = new Texture("images/background.jpg");
+		brickwall = new Texture("images/brickwall.jpg");
+		
 		brickPositions = new Integer[13][29];
+		for (int i = 0; i < 13; i++) {
+			for (int j = 0; j < 29; j++) {
+				brickPositions[i][j] = 0;
+			}
+		}
 		
 		//INITIALISING THE POSITIONS OF BRICKS
 		Random rndSpawner = new Random();
 		
 		for (int i = 0; i < 13; i++) {
+			//NO BRICKS MUST BE SPAWNED WHERE THE PLAYER IS
+			if(i >= 11){
+				if(i%2 == 0){
+					//IF ITS A BLANK LINE
+					for (int j = 2; j < 29; j++) {
+						int shouldSpawn = rndSpawner.nextInt(3);
+						if(shouldSpawn > 0){
+							brickPositions[i][j] = 1;
+						}
+					}
+				}
+				else{
+					//IF ITS A LINE WITH WALLS
+					for (int j = 2; j < 29; j+=2) {
+						int shouldSpawn = rndSpawner.nextInt(3);
+						if(shouldSpawn > 0){
+							brickPositions[i][j] = 1;
+						}
+					}
+				}
+				continue;
+			}
+			
 			if(i%2 == 0){
 				//IF ITS A BLANK LINE
 				for (int j = 0; j < 29; j++) {
-					int shouldSpawn = rndSpawner.nextInt(2);
+					int shouldSpawn = rndSpawner.nextInt(3);
 					if(shouldSpawn > 0){
 						brickPositions[i][j] = 1;
-					}
-					else{
-						brickPositions[i][j] = 0;
 					}
 				}
 			}
 			else{
 				//IF ITS A LINE WITH WALLS
 				for (int j = 0; j < 29; j+=2) {
-					int shouldSpawn = rndSpawner.nextInt(10);
+					int shouldSpawn = rndSpawner.nextInt(3);
 					if(shouldSpawn > 0){
 						brickPositions[i][j] = 1;
-					}
-					else{
-						brickPositions[i][j] = 0;
 					}
 				}
 			}
@@ -110,7 +130,7 @@ public class Engine extends ApplicationAdapter {
 			}
 			else{
 				//IF ITS A LINE WITH WALLS
-				for (int j = 0; j < 29; j+=2) {
+				for (int j = 0; j < 28; j+=2) {
 					//[i - 40 / 40][j - 100 / 40]
 					if(brickPositions[i][j] > 0){
 						batch.draw(brickwall, (j * 40) + 100, (i * 40) + 40);
